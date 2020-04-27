8df5ff27d968b93493651a83757f5625113526cf
kourosh-safari <46763611+kourosh-safari@users.noreply.github.com>
2020-04-27 14:52:34 -0700

    Redesigned site for bot

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 function AppFooter() {
   return (
     <Container>
-      This is a demo of a minimal Next.js app. Check out the source code on{" "}
+      This is a placeholder webpage for WIP Slackbot. Source Code:{" "}
       <a href="https://github.com/ucsb-cs48-s20/demo-nextjs-app">GitHub</a>!
     </Container>
   );
