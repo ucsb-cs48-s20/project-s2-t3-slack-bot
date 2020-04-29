0c3deab534df864ab7cafb25d712a58844a9eb38
Alan Zhang <alan.zhang.052@gmail.com>
2020-04-28 21:42:30 -0700

    Fixed hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const request = require("request");
 
 export default async function (req, res) {
@@ -7,8 +8,11 @@ export default async function (req, res) {
     form: {
       token: process.env.SLACK_AUTH_TOKEN,
       channel: "#general",
-      text: "Hello!",
+      text: "Hi! :wave: \n I'm your new bot.",
     },
   };
   await request.post("https://slack.com/api/chat.postMessage", data);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ text }));
 }
