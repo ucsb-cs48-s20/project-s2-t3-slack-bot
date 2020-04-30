548adecca7f4036e7f6846b3c5d09c4bb91ebb4b
Alan Zhang <alan.zhang.052@gmail.com>
2020-04-30 16:49:29 -0700

    Implemented foundation for MVP functions

---
+++ pages/api/appraise.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const request = require("request");
+
+export default async function (req, res) {
+  console.log("PRINT");
+  var data = {
+    form: {
+      token: process.env.SLACK_AUTH_TOKEN,
+      channel: "#general",
+      text: "Hi! :wave: \n I'm your new bot.",
+    },
+  };
+  await request.post("https://slack.com/api/chat.postMessage", data);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ text }));
+}
