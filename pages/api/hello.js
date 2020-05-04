2ff0e461c5650d3f1289e453ddd4b2d0833f8868
Alan Zhang <alan.zhang.052@gmail.com>
2020-05-03 20:58:26 -0700

    Set up final variations of in progress commands

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,6 @@
 const request = require("request");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.post("/", (req, res) => {
-  var data = {
-    form: {
-      token: process.env.SLACK_AUTH_TOKEN,
-      channel: "#testing",
-      text: "/hello is back online!",
-    },
-  };
-  request.post("https://slack.com/api/chat.postMessage", data, function (
-    error,
-    response,
-    body
-  ) {
-    // Sends welcome message
-    res.json();
-  });
-});
+export default async function (req, res) {
+  // Sends hello to user personally
+  res.end("Hello!");
+}
