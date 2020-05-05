1c85f615dfcf01666bb55a8037e928e40858c9a7
Adarsha Poudel <adarsha@ucsb.edu>
2020-05-05 15:52:44 -0700

    ap - added functionality for praise

--- pages/api/praise.js
+++ pages/api/praise.js
@@ -3,9 +3,37 @@ const request = require("request");
 import { initDatabase } from "../../utils/mongodb";
 
 export default async function (req, res) {
-  if (req.body.text) {
-    res.end(req.body.text + " has been praised");
-    console.log(req.body.text);
-    //const client = await initDatabase();
+  let userName = req.body.text;
+
+  if (!userName || userName.trim() === "") {
+    res.end("Please tag the person you want to praise :)");
+  } else {
+    res.end(userName.slice(1) + " has been praised.");
+
+    const client = await initDatabase();
+    const usersCollection = client.collection("users");
+    const query = await usersCollection.findOne({ name: userName });
+
+    if (query) {
+      usersCollection
+        .updateOne(query, { $set: { praiseValue: query.praiseValue + 1 } })
+        .then(() =>
+          console.log(`Successfully updated item with _id: ${query._id}`)
+        )
+        .catch((err) => console.error(`Failed to update item: ${err}`));
+    } else {
+      const newUser = {
+        name: userName,
+        praiseValue: 1,
+      };
+      usersCollection
+        .insertOne(newUser)
+        .then((result) =>
+          console.log(
+            `Successfully inserted item with _id: ${result.insertedId}`
+          )
+        )
+        .catch((err) => console.error(`Failed to insert item: ${err}`));
+    }
   }
 }
