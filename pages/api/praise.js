20a0fd8432cab7ceee940ce572d1164d73c80811
Adarsha Poudel <adarsha@ucsb.edu>
2020-05-05 17:43:43 -0700

    ap - fixed praise.js

--- pages/api/praise.js
+++ pages/api/praise.js
@@ -8,32 +8,34 @@ export default async function (req, res) {
   if (!userName || userName.trim() === "") {
     res.end("Please tag the person you want to praise :)");
   } else {
-    res.end(userName.slice(1) + " has been praised.");
-
     const client = await initDatabase();
     const usersCollection = client.collection("users");
     const query = await usersCollection.findOne({ name: userName });
 
     if (query) {
-      usersCollection
-        .updateOne(query, { $set: { praiseValue: query.praiseValue + 1 } })
-        .then(() =>
-          console.log(`Successfully updated item with _id: ${query._id}`)
-        )
-        .catch((err) => console.error(`Failed to update item: ${err}`));
+      try {
+        await usersCollection.updateOne(query, {
+          $set: { praiseValue: query.praiseValue + 1 },
+        });
+        console.log(`Successfully updated item with _id: ${query._id}`);
+      } catch (err) {
+        console.error(`Failed to update item: ${err}`);
+      }
     } else {
       const newUser = {
         name: userName,
         praiseValue: 1,
       };
-      usersCollection
-        .insertOne(newUser)
-        .then((result) =>
-          console.log(
-            `Successfully inserted item with _id: ${result.insertedId}`
-          )
-        )
-        .catch((err) => console.error(`Failed to insert item: ${err}`));
+      try {
+        await usersCollection.insertOne(newUser);
+        console.log(
+          `Successfully inserted item with _id: ${result.insertedId}`
+        );
+      } catch (err) {
+        console.error(`Failed to insert item: ${err}`);
+      }
+
+      res.end(userName.slice(1) + " has been praised.");
     }
   }
 }
