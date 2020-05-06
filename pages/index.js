7608b219c75d2afb214a143f9053b6d3eccb2f31
Tommy Chao <tchao@ucsb.edu>
2020-05-06 02:29:16 -0700

    tc - added bot invite button to webpage

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,20 @@ function HomePage(props) {
 
   return (
     <Layout user={user}>
+      <div>
+        Add the bot to your Slack workspace by clicking the button below!
+      </div>
+      <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands,chat:write&client_id=1089398914164.1087443355106">
+        <img
+          alt="Add to Slack"
+          height="40"
+          width="139"
+          src="https://platform.slack-edge.com/img/add_to_slack.png"
+          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+        />
+      </a>
+      <br></br>
+      <br></br>
       {user ? (
         <div>
           You're logged in! Here's what the server knows about you:
@@ -16,6 +30,7 @@ function HomePage(props) {
       ) : (
         <div>You're not logged in!</div>
       )}
+      <br></br>
     </Layout>
   );
 }
