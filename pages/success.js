fff667167dd4d4e35f5453a7aeb867ee7f990df1
Tommy Chao <tchao@ucsb.edu>
2020-05-08 03:11:36 -0700

    tc - added a page to redirect to after inviting bot to workspace

--- pages/success.js
+++ pages/success.js
@@ -8,18 +8,7 @@ function HomePage(props) {
 
   return (
     <Layout user={user}>
-      <div>
-        Add our bot to your Slack workspace by clicking the button below!
-      </div>
-      <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands,chat:write&client_id=1089398914164.1087443355106">
-        <img
-          alt="Add to Slack"
-          height="40"
-          width="139"
-          src="https://platform.slack-edge.com/img/add_to_slack.png"
-          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-        />
-      </a>
+      <div>The bot has been added to your workspace!</div>
       <br></br>
       <br></br>
       {user ? (
