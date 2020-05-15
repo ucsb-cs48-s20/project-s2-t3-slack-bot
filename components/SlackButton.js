e4760dcf4777546b069f1416bd2287f5a35a9cd9
Adarsha Poudel <adarsha@ucsb.edu>
2020-05-14 17:59:19 -0700

    ap - added react component

---
+++ components/SlackButton.js
@@ -0,0 +1,22 @@
+import Container from "react-bootstrap/Container";
+
+function SlackButton() {
+  return (
+    <>
+      <div>
+        Add our bot to your Slack workspace by clicking the button below!
+      </div>
+      <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands,chat:write&client_id=1089398914164.1087443355106">
+        <img
+          alt="Add to Slack"
+          height="40"
+          width="139"
+          src="https://platform.slack-edge.com/img/add_to_slack.png"
+          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+        />
+      </a>
+    </>
+  );
+}
+
+export default SlackButton;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import SlackButton from "../components/SlackButton";
 import { optionalAuth } from "../utils/ssr";
 
 export const getServerSideProps = optionalAuth;
@@ -8,19 +9,7 @@ function HomePage(props) {
 
   return (
     <Layout user={user}>
-      <div>
-        Add our bot to your Slack workspace by clicking the button below!
-      </div>
-
-      <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands,chat:write&client_id=1089398914164.1087443355106">
-        <img
-          alt="Add to Slack"
-          height="40"
-          width="139"
-          src="https://platform.slack-edge.com/img/add_to_slack.png"
-          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-        />
-      </a>
+      <SlackButton></SlackButton>
       <br></br>
       <br></br>
       {user ? (
