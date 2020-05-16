6467667d07623f1a72fbbabb1caa00b556dcab2d
Alan Zhang <alan.zhang.052@gmail.com>
2020-05-16 01:18:22 -0700

    Foundation for ranking list

---
+++ pages/api/rankings.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const request = require("request");
+import { initDatabase } from "../../utils/mongodb";
+
+export default async function (req, res) {
+  let userName = req.body.text;
+  const client = await initDatabase();
+  const usersCollection = client.collection("users");
+  const query = await usersCollection.findOne({ name: userName });
+  if (query.praiseValue != 0) {
+    console.log("Successfully found user");
+    res.end(
+      userName.slice(1) + " has " + query.praiseValue.toString(10) + " rep!"
+    );
+  } else {
+    res.end(userName.slice(1) + " has no rep. :(");
+  }
+}
