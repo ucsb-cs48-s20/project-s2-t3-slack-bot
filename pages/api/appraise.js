a6b83e38ce8a7ca143eb7f62362fc7724482ccdd
Alan Zhang <alan.zhang.052@gmail.com>
2020-05-15 22:22:10 -0700

    Prettier fix?

--- pages/api/appraise.js
+++ pages/api/appraise.js
@@ -4,14 +4,12 @@ import { initDatabase } from "../../utils/mongodb";
 
 export default async function (req, res) {
   let userName = req.body.text;
-
   if (!userName || userName.trim() === "") {
     res.end("Please tag the person you want to view :)");
   } else {
     const client = await initDatabase();
     const usersCollection = client.collection("users");
     const query = await usersCollection.findOne({ name: userName });
-
     if (query.praiseValue != 0) {
       console.log("Successfully found user");
       res.end(
