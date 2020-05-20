b9e8a308de779d5fa809ab94cec7c8066b873bcd
Adarsha Poudel <adarsha@ucsb.edu>
2020-05-19 18:18:22 -0700

    ap- fetch origin

--- pages/api/praise.js
+++ pages/api/praise.js
@@ -6,20 +6,25 @@ export default async function (req, res) {
   let userName = req.body.text.slice(1);
   var timeStamp = Math.floor(Date.now() / 1000);
 
+  //checking if ther user is trying to praise himself
   if (userName == req.body.user_name)
     res.end("You cannot praise yourself, silly.");
 
+  //checking if the name of the person the user wants to praise is an empty string
   if (!userName || userName.trim() === "") {
     res.end("Please tag the person you want to praise :)");
   } else {
+    //if not empty initiazlize mongodbdatabase and get the usercollection to access it.
     const client = await initDatabase();
     const usersCollection = client.collection("users");
     const query = await usersCollection.findOne({ name: userName });
     const query2 = await usersCollection.findOne({ name: req.body.user_name });
 
     if (query2) {
-      var lastPraised = query2.lastPraiseTime;
+      //if you are in database lets get the time you last praised
+      var lastPraised = query2.lastPraiseTime; //this is last time the user praise someone
       if (timeStamp - lastPraised < 20) {
+        // if user has already praisied in last 20 seconds wait for some time to praise again
         console.log(`Time difference: ${timeStamp - lastPraised}`);
         res.end(`Wait ${20 - timeStamp + lastPraised} seconds to praise again`);
       } else {
@@ -27,7 +32,9 @@ export default async function (req, res) {
           $set: { lastPraiseTime: timeStamp },
         });
 
+        //if you have not praised in last 20 seconds you are ready to either add a new user with praiseValue 1 or increment their praiseValue
         if (query) {
+          //if user exits increment their praiseValue
           try {
             await usersCollection.updateOne(query, {
               $set: { praiseValue: query.praiseValue + 1 },
@@ -38,6 +45,7 @@ export default async function (req, res) {
             console.error(`Failed to update item: ${err}`);
           }
         } else {
+          //if not create a new user you want to praise with praiseValue 1
           const newUser2 = {
             name: userName,
             praiseValue: 1,
@@ -54,6 +62,7 @@ export default async function (req, res) {
         }
       }
     } else {
+      //if you are not in database let add you to mongodb database with the timeStamp which is current time
       const newUser = {
         name: req.body.user_name,
         praiseValue: 0,
